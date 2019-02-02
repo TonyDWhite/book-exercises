355fced8c654a090d959c96060a4a3ea70895cd3
TonyWang <allaccounts@yeah.net>
2019-02-02 12:22:33 -0800

    recommited exercise 6

--- chapter-11-exercises/exercise-6/exercise.R
+++ chapter-11-exercises/exercise-6/exercise.R
@@ -5,14 +5,26 @@
 #install.packages("nycflights13")  # should be done already
 library("nycflights13")
 library("dplyr")
-
+View(flights)
 # Create a dataframe of the average arrival delays for each _destination_, then 
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
+ave_arr_delay_dest <- group_by(flights, dest) %>% 
+  summarize(ave_arr_delay = mean(arr_delay, na.rm = TRUE))
 
+airports <- left_join(airports, ave_arr_delay_dest, by = c("faa" = "dest"))#watch that it's = here!
 
+airports %>% 
+filter(ave_arr_delay == max(ave_arr_delay, na.rm = TRUE)) %>%  
+  pull(name)
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
-
+airlines <- group_by(flights, carrier) %>% 
+  summarize(ave_delay_airline = mean(arr_delay, na.rm = TRUE)) %>% 
+  right_join(airlines, by = "carrier") # used right join so don't need another variable, but the row order is wrong
+#OR
+ave_carrier_delay <- group_by(flights, carrier) %>% 
+  summarize(ave_delay_airline = mean(arr_delay, na.rm = TRUE))
+airlines <- left_join(airlines, ave_carrier_delay, by = "carrier")
--- chapter-12-exercises/exercise-1/exercise.R
+++ chapter-12-exercises/exercise-1/exercise.R
@@ -1,33 +1,38 @@
 # Exercise 1: analyzing avocado sales with the `tidyr` package
-
+#install.packages("tidyr")
+#install.packages("ggplot2")
 # Load necessary packages (`tidyr`, `dplyr`, and `ggplot2`)
-
+library(tidyr)
+library(dplyr)
+library(ggplot2)
 
 # Set your working directory using the RStudio menu:
 # Session > Set Working Directory > To Source File Location
 
 # Load the `data/avocado.csv` file into a variable `avocados`
 # Make sure strings are *not* read in as factors
-
+avocados <- read.csv("data/avocado.csv")
 
 # To tell R to treat the `Date` column as a date (not just a string)
 # Redefine that column as a date using the `as.Date()` function
 # (hint: use the `mutate` function)
-
+avocados <- mutate(avocados, Date = as.Date(Date))
 
 # The file had some uninformative column names, so rename these columns:
 # `X4046` to `small_haas`
 # `X4225` to `large_haas`
 # `X4770` to `xlarge_haas`
-
+colnames(avocados)[colnames(avocados) == "X4046"] = "small_haas"
+colnames(avocados)[colnames(avocados) == "X4225"] = "large_haas"
+colnames(avocados)[colnames(avocados) == "X4770"] = "xlarge_haas"
 
 # The data only has sales for haas avocados. Create a new column `other_avos`
 # that is the Total.Volume minus all haas avocados (small, large, xlarge)
-
+avocados <- mutate(avocados, other_avos = Total.Volume - small_haas - large_haas - xlarge_haas)
 
 # To perform analysis by avocado size, create a dataframe `by_size` that has
 # only `Date`, `other_avos`, `small_haas`, `large_haas`, `xlarge_haas`
-
+by_size <- select(avocados, Date, other_avos, small_haas, large_haas, xlarge_haas)
 
 # In order to visualize this data, it needs to be reshaped. The four columns
 # `other_avos`, `small_haas`, `large_haas`, `xlarge_haas` need to be 
